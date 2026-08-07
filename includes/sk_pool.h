3c8a0c1ec74e0428a66273000f00972f3a203d29
Sky11y <sky11y1991@gmail.com>
2026-08-07 11:26:08 +0300

    Basic implementation of pool.

--- includes/sk_pool.h
+++ includes/sk_pool.h
@@ -1,7 +1,10 @@
 #ifndef SK_POOL_H
  #define SK_POOL_H
 
+#include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
 typedef struct free_list free_list;
 typedef struct sk_pool sk_pool;
@@ -10,16 +13,19 @@ struct sk_pool {
     unsigned char*  buf;        // beginning of the pool buffer
     size_t          capacity;   // total capacity of the buffer
     size_t          chunk_size; // Size of one chunk
-    size_t          chunks;     // how many chunks alloced
-    size_t          free;       // how many chunks free
-    int             flags;      // possible flags
-
+    size_t          alloced_chunks;     // how many chunks alloced
+    size_t          free_chunks;       // how many chunks free
     free_list*      head;       // first free chunk
+    int             flags;      // possible flags
 };
 
 struct free_list {
     free_list*  next;
 };
+
+#define USER_BUF 0x00000001
+#define DEFAULT_COUNT 42
+#define DEFAULT_SIZE 8
 
 /*
    PROTOTYPES
@@ -28,10 +34,10 @@ bool        sk_pool_init(sk_pool* pool, size_t object_size, size_t object_count)
 bool        sk_pool_init_with_buffer(sk_pool* pool, void* buffer, size_t object_size, size_t object_count);
 bool        sk_pool_destroy(sk_pool* pool); // destroy the whole pool
 void*       sk_pool_alloc(sk_pool* pool); // allocate one object
-void        sk_pool_free(sk_pool* pool, void* ptr); // free one object
+bool        sk_pool_free(sk_pool* pool, void* ptr); // free one object
 void        sk_pool_reset(sk_pool* pool); // free all the objects
-size_t      sk_pool_capacity(sk_pool* pool); // total number of objects
-size_t      sk_pool_count(sk_pool* pool); // objects alloced
-size_t      sk_pool_remaining(sk_pool* pool); // free objects left
+ssize_t      sk_pool_capacity(sk_pool* pool); // total number of objects
+ssize_t      sk_pool_count(sk_pool* pool); // objects alloced
+ssize_t      sk_pool_remaining(sk_pool* pool); // free objects left
 
 #endif
---
+++ src/sk_pool.c
@@ -0,0 +1,111 @@
+#include "../includes/sk_pool.h"
+
+bool sk_pool_init(sk_pool* pool, size_t object_size, size_t object_count)
+{
+    if (!pool)
+        return false;
+
+    pool->chunk_size = object_size ? object_size : DEFAULT_SIZE;
+    pool->free_chunks = object_count ? object_count : DEFAULT_COUNT;
+    pool->capacity = pool->chunk_size * pool->free_chunks;
+    pool->buf = malloc(pool->capacity);
+    assert(pool->buf);
+    pool->alloced_chunks = 0;
+    pool->head = NULL;
+    pool->flags = 0;
+    return true;
+}
+
+bool sk_pool_init_with_buffer(sk_pool* pool, void* buffer, size_t object_size, size_t object_count)
+{
+    if (!pool || !buffer)
+        return false;
+
+    pool->buf = buffer;
+    pool->chunk_size = object_size ? object_size : DEFAULT_SIZE;
+    pool->free_chunks = object_count ? object_count : DEFAULT_COUNT;
+    pool->capacity = pool->chunk_size * pool->free_chunks;
+    pool->alloced_chunks = 0;
+    pool->head = NULL;
+    pool->flags = USER_BUF;
+    return true;
+}
+
+bool sk_pool_destroy(sk_pool* pool)
+{
+    if (!pool)
+        return false;
+
+    if ((0xFF & pool->flags) != USER_BUF)
+        free(pool->buf);
+
+    memset((void*)pool, 0, sizeof(*pool));
+
+    return true;
+}
+
+void* sk_pool_alloc(sk_pool* pool)
+{
+    void* ptr = NULL;
+    if (!pool || pool->free_chunks == 0)
+        return ptr;
+
+    if (pool->head) {
+        ptr = pool->head;
+        pool->head = pool->head->next;
+    } else {
+        ptr = pool->buf + pool->alloced_chunks * pool->chunk_size;
+    }
+
+    ++pool->alloced_chunks;
+    --pool->free_chunks;
+
+    return ptr;
+}
+
+bool sk_pool_free(sk_pool* pool, void* ptr)
+{
+    if (!pool || !ptr)
+        return false;
+
+    free_list *node = (free_list*)ptr;
+    node->next= pool->head;
+    pool->head = node;
+
+    return true;
+}
+
+void sk_pool_reset(sk_pool* pool)
+{
+    if (!pool)
+        return;
+
+    pool->alloced_chunks = 0;
+    pool->free_chunks = pool->capacity / pool->chunk_size;
+    pool->head = NULL;
+}
+
+ssize_t sk_pool_capacity(sk_pool* pool)
+{
+    if (!pool)
+        return -1;
+
+    return pool->capacity;
+}
+
+ssize_t sk_pool_count(sk_pool* pool)
+{
+    if (!pool)
+        return -1;
+
+    return pool->alloced_chunks;
+}
+
+ssize_t sk_pool_remaining(sk_pool* pool)
+{
+    if (!pool)
+        return -1;
+
+    return pool->free_chunks;
+}
+
